c08499dbaa62bea7e5acb75607c638d550e72a23
Sreeraj S Kurup <154326486+s-r-e-e-r-a-j@users.noreply.github.com>
2025-10-27 13:35:34 +0530

    Update crypto.py

--- crypto.py
+++ crypto.py
@@ -2,16 +2,23 @@
 # GitHub: https://github.com/s-r-e-e-r-a-j
 
 from __future__ import annotations
-import os, base64, json
+import os, base64
 from typing import Dict
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 class Crypto:
-    def __init__(self, passphrase: str, salt: bytes | None = None, iterations: int = 200_000) -> None:
+    def __init__(self, passphrase: str, salt_file: str | None = None, iterations: int = 200_000) -> None:
         self.iterations = iterations
-        self.salt = salt or os.urandom(16)
+        self.salt_file = salt_file or "pylitedb.salt"
+        if os.path.exists(self.salt_file):
+            with open(self.salt_file, "r", encoding="utf-8") as f:
+                self.salt = base64.b64decode(f.read().strip())
+        else:
+            self.salt = os.urandom(16)
+            with open(self.salt_file, "w", encoding="utf-8") as f:
+                f.write(base64.b64encode(self.salt).decode())
         kdf = PBKDF2HMAC(algorithm=hashes.SHA3_256(), length=32, salt=self.salt, iterations=self.iterations)
         self.key = kdf.derive(passphrase.encode())
 
